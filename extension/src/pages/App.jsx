ee45de45d4475e1772b37bf6fbcc52cf90b154bb
47230 Miguel Neves <A47230@alunos.isel.pt>
2024-04-11 15:00:54 +0100

    file structure reworked

---
+++ extension/src/pages/App.jsx
@@ -0,0 +1,25 @@
+
+
+const App = () => {
+
+    return (
+        <main>
+            <h1>Phising Net</h1>
+            <button onClick={()=>{
+                console.log('Tried to login');
+                //fetch('get token to API')
+            }}>
+                Login
+            </button>
+            <button onClick={()=>{
+                console.log('Tried to sign up');
+                //window.open('sign up SPA url','_blank')
+            }}>
+                Sign Up
+            </button>
+        </main>
+    )
+}
+
+
+export default App;
--- frontend/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import App from './pages/App';
-import React from 'react';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
