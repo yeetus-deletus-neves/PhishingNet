e9fff8eb39f9e1c26175798e6ef352fa2e7751be
Tiago Pardal <a47206@alunos.isel.pt>
2024-04-20 18:54:17 +0100

    cache attempt

--- frontend/src/components/Link.jsx
+++ frontend/src/components/Link.jsx
@@ -2,8 +2,25 @@ import { defaultFetch } from "../../../extension/src/utils/fetch";
 
 export async function LinkPage(){
 
+    const saveToCache = async (cacheName, key, value) => {
+        try {
+            const data = new Response(JSON.stringify(value));
+            
+            caches.open(cacheName).then((cache) => {
+                cache.put(key, data);
+            });
+            //await cache.put(key, data); 
+            console.log(`Data saved to cache: ${value}`);
+        } catch (error) {
+            console.error('Error saving data to cache:', error);
+        }
+    };
+
+
     const searchParams = new URLSearchParams(window.location.search)
     const azureCode = searchParams.get('code')
+    saveToCache("tsest", "code", azureCode);
+
     if(azureCode){
         const linkRsp = await defaultFetch(
             'http://localhost:8080/user',
