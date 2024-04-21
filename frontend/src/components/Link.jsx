6fe884747d53af1fbe2f03f9147feb816818a7e0
Tiago Pardal <a47206@alunos.isel.pt>
2024-04-21 03:15:16 +0100

    Cache implemented

--- frontend/src/components/Link.jsx
+++ frontend/src/components/Link.jsx
@@ -1,8 +1,8 @@
 import { defaultFetch } from "../../../extension/src/utils/fetch";
 
-export async function LinkPage(){
+export function LinkPage(){
 
-    const saveToCache = async (cacheName, key, value) => {
+    async function saveToCache(cacheName, key, value) {
         try {
             const data = new Response(JSON.stringify(value));
             
@@ -16,24 +16,49 @@ export async function LinkPage(){
         }
     };
 
+    async function getFromCache(cacheName, key) {
+        try {
+            const cache = await caches.open(cacheName);
+            const cachedResponse = await cache.match(key);
+      
+            if (cachedResponse) {
+              const data = await cachedResponse.text();
+              console.log(`Data retrieved from cache with key "${key}":`, data);
+              return data;
+            } else {
+              console.log(`No data found in cache with key "${key}".`);
+              return null;
+            }
+        } catch (error) {
+            console.error('Error retrieving data from cache:', error);
+            return null;
+        }
+      }
 
     const searchParams = new URLSearchParams(window.location.search)
     const azureCode = searchParams.get('code')
-    saveToCache("tsest", "code", azureCode);
-
-    if(azureCode){
-        const linkRsp = await defaultFetch(
-            'http://localhost:8080/user',
-            "POST",
-            {
-                'Content-Type': 'application/json',
-                'Bearer Token': '<search in cache, or get through provider>',
-            },
-            {
+    /saveToCache("test", "code", azureCode);
+    let t;
+    (async () => {
+        t = getFromCache("test", "code");
+    })();
+    console.log(this);
+    
+    /*if(azureCode){
+        (async () => {
+            const linkRsp = await defaultFetch(
+                'http://localhost:8080/user',
+                "POST",
+                {
+                    'Content-Type': 'application/json',
+                    'Bearer Token': '<search in cache, or get through provider>',
+                },
+                {
                 "token": azureCode
-            }
-        )
-    }
+                }
+            )
+        })();
+    }*/
 
     return (
     <div className="container">
