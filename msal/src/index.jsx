9cf1233fb9c23b17f49a36502274a52d8b1911be
yeetus-deletus-neves <miguelassuda123@gmail.com>
2024-05-02 11:29:00 +0100

    not needed anymore
    was only in repo for referencing

--- msal/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import App from './pages/App';
-import React from 'react';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
-
--- msal/src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function HomePage(){
-    return(
-        <div>
-          <p>User authenticated</p>  
-        </div>
-    )
-}
